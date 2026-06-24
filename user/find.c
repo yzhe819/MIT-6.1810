1723304d0170294c8a14faceb8840493ff62bec4
yzhe819 <68207314+yzhe819@users.noreply.github.com>
2026-06-25 02:39:11 +1200

    add: better `find`

--- user/find.c
+++ user/find.c
@@ -7,14 +7,27 @@
 
 // get the last file name
 // ./a/b/hello.txt -> hello.txt
-char*
-fmtname(char *path)
+char *
+basename(char *path)
 {
-  char *p;
-  for(p=path+strlen(path); p >= path && *p != '/'; p--)
-    ;
-  p++;
-  return p;
+  char *p = path + strlen(path);
+  while (p >= path && *p != '/') p--;
+  return p + 1;
+}
+
+void
+invoke(char **exec_args, int exec_argc, char *file)
+{
+  char *args[MAXARG];
+  memmove(args, exec_args, exec_argc * sizeof(char*));
+  args[exec_argc] = file;
+  args[exec_argc + 1] = 0;
+  if (fork() == 0) {
+    exec(args[0], args);
+    fprintf(2, "find: exec %s failed\n", args[0]);
+    exit(1);
+  }
+  wait(0);
 }
 
 void
@@ -25,39 +38,16 @@ find(char *path, char *target, char **exec_args, int exec_argc)
   struct dirent de;
   struct stat st;
 
-  if((fd = open(path, O_RDONLY)) < 0){
+  if ((fd = open(path, O_RDONLY)) < 0 || fstat(fd, &st) < 0) {
     fprintf(2, "find: cannot open %s\n", path);
-    return;
-  }
-
-  if(fstat(fd, &st) < 0){
-    fprintf(2, "find: cannot stat %s\n", path);
     close(fd);
     return;
   }
 
   switch(st.type){
   case T_FILE:
-    if(strcmp(fmtname(path), target) == 0){
-      if(exec_args != 0){
-        char *args[MAXARG];
-        int i;
-        for(i = 0; i < exec_argc; i++)
-          args[i] = exec_args[i];
-        args[exec_argc] = path;
-        args[exec_argc + 1] = 0;
-
-        int pid = fork();
-        if(pid == 0){
-          exec(args[0], args);
-          fprintf(2, "find: exec %s failed\n", args[0]);
-          exit(1);
-        }
-        wait(0);
-      } else {
-        printf("%s\n", path);
-      }
-    }
+    if (strcmp(basename(path), target) == 0)
+      exec_args ? invoke(exec_args, exec_argc, path) : printf("%s\n", path);
     break;
 
   case T_DIR:
@@ -65,38 +55,34 @@ find(char *path, char *target, char **exec_args, int exec_argc)
       fprintf(2, "find: path too long\n");
       break;
     }
+
     strcpy(buf, path);
     p = buf + strlen(buf);
     *p++ = '/';
+
     while(read(fd, &de, sizeof(de)) == sizeof(de)){
-      if(de.inum == 0)
-        continue;
-      if(strcmp(de.name, ".") == 0 || strcmp(de.name, "..") == 0)
+      if (de.inum == 0 || !strcmp(de.name, ".") || !strcmp(de.name, ".."))
         continue;
       memmove(p, de.name, DIRSIZ);
       p[DIRSIZ] = 0;
-      if(stat(buf, &st) < 0){
-        fprintf(2, "find: cannot stat %s\n", buf);
-        continue;
-      }
-      find(buf, target, exec_args, exec_argc);
+      find(buf, target, exec_args, exec_argc); // recursive invoke the find method
     }
     break;
   }
+
   close(fd);
 }
 
 int
 main(int argc, char *argv[])
 {
-  if(argc == 3){
+  if (argc == 3) {
     find(argv[1], argv[2], 0, 0);
-    exit(0);
-  }
-  if(argc >= 5 && strcmp(argv[3], "-exec") == 0){
+  } else if (argc >= 5 && strcmp(argv[3], "-exec") == 0) {
     find(argv[1], argv[2], &argv[4], argc - 4);
-    exit(0);
+  } else {
+    fprintf(2, "Usage: find <dir> <file> [-exec <cmd> [args...]]\n");
+    exit(1);
   }
-  fprintf(2, "Usage: find <dir> <file> [-exec <cmd> [args...]]\n");
-  exit(1);
+  exit(0);
 }
