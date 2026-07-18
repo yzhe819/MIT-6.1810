e0b729349257afcc40fd55d9b8bdd867c90e20d4
yzhe819 <68207314+yzhe819@users.noreply.github.com>
2026-07-18 18:48:43 +1200

    add: `symlink`

--- kernel/sysfile.c
+++ kernel/sysfile.c
@@ -511,5 +511,29 @@ sys_pipe(void)
 uint64
 sys_symlink(void)
 {
-  return 0
+  char target[MAXPATH], path[MAXPATH];
+  struct inode* ip;
+
+  if(argstr(0, target, MAXPATH) < 0 || argstr(1, path, MAXPATH) < 0)
+    return -1;
+
+  begin_op();
+
+  // create a new inode
+  if((ip = create(path, T_SYMLINK, 0, 0)) == 0) {
+    end_op();
+    return -1;
+  }
+
+  if(writei(ip, 0, (uint64)target, 0, strlen(target) + 1) !=
+     strlen(target) + 1) {
+    iunlockput(ip);
+    end_op();
+    return -1;
+  }
+
+  iunlockput(ip);
+  end_op();
+
+  return 0;
 }
